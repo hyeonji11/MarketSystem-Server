4a0b2f134c6509b16dadf04426a0b4b0acc8b5de
hyeonji <hyeonji.27@daum.net>
2021-05-13 13:00:17 +0900

    거래 파라미터 타입 수정

--- src/main/java/com/ms/controller/ItemController.java
+++ src/main/java/com/ms/controller/ItemController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -127,7 +126,7 @@ public class ItemController {
 	}
 
 	@PostMapping("/trans")
-	public ResponseEntity<?> transactionComplete(@RequestBody Integer chatRoomIdx) {
+	public ResponseEntity<?> transactionComplete(Integer chatRoomIdx) {
 		transService.transactionSave(chatRoomIdx);
 		return new ResponseEntity("거래가 완료되었습니다.", HttpStatus.OK);
 	}
